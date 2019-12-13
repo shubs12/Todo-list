4793e333db0556992c35927b34b6df228b500cf9
shubs12 <jungharesj7@gmail.com>
2019-12-13 23:54:43 +0530

    todo

---
+++ todo/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <todo/>
+  </div>
+</template>
+
+<script>
+import todo from './components/todo'
+
+export default {
+  name: 'App',
+  components: {
+    todo
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ todo/todo/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <todo></todo>
+  </div>
+</template>
+
+<script>
+import todo from './components/todo'
+export default {
+  name: 'App',
+  components: {
+    todo
+  }
+}
+</script>
+
+<style>
+</style>
